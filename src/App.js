7329f52bf9ad323207f4e40ffd8e942084388271
Teatoller <steven.otieno1@gmail.com>
2019-07-23 10:20:46 +0300

    (event): implement event on input box
    - add an onChange event on the input
    - create an onchange function
    
    [Starts #014]

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import TodoItem from "./TodoItem";
 import todosData from "./todosData";
 
 class App extends Component {
   constructor() {
-    super()
+    super();
     this.state = {
-      todo: todosData
-    }
+      todos: todosData
+    };
+    this.handleChange = this.handleChange.bind(this);
   }
-  render(){
-    const todosItems = this.state.todo.map(task => (
-      <TodoItem key={task.id} text={task.text} completed={task.completed} />
+
+  handleChange(id) {
+    console.log("hc...", id);
+  }
+
+  render() {
+    const todosItems = this.state.todos.map(item => (
+      <TodoItem key={item.id} item={item} handleChange={this.handleChange} />
     ));
-    return (
-      <div className="todo-list">
-        {todosItems}
-      </div>
-    );
+
+    return <div className="todo-list">{todosItems}</div>;
   }
-  
 }
 
 export default App;
--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,13 +1,17 @@
-import React from "react"
+import React from "react";
 
 function TodoItem(props) {
-    return (
-        <div className="todo-item" >
-            <input type="checkbox"  />
-            {props.text}
-            <p>{props.completed}</p>
-        </div>
-    )
+  return (
+    <div className="todo-item">
+      <input
+        type="checkbox"
+        checked={props.item.completed}
+        onChange={() => props.handleChange(props.item.id)}
+      />
+      {props.text}
+      <p>{props.item.text}</p>
+    </div>
+  );
 }
 
-export default TodoItem
+export default TodoItem;
